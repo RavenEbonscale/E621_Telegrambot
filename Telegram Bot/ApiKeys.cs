40045ffe94dbc30f8cf501c2f08d90486d71da98
RavenEbonscale <68269041+RavenEbonscale@users.noreply.github.com>
2020-11-11 14:43:25 -0500

    Update ApiKeys.cs

--- Telegram Bot/ApiKeys.cs
+++ Telegram Bot/ApiKeys.cs
@@ -7,8 +7,8 @@ namespace Telegram_Bot
      internal class ApiKeys
     {
         public readonly string ApiKeyE621 = "Api-key-here";
-        public readonly string username = "Username-here";
-        public readonly string useragent = "User-agent-here";
+        public readonly string username = "UsernameE621-here";
+        public readonly string useragent = "UserE621-agent-here";
         public readonly string ApiKeytele = "Api-key-here";
     }
 }
