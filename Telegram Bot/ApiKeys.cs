1c31a302223fe0ecd4ff3213ae57f7a461a8fa03
RavenEbonscale <createhavoc@hotmail.com>
2020-12-16 22:37:23 -0500

    cleared things up:

--- Telegram Bot/ApiKeys.cs
+++ Telegram Bot/ApiKeys.cs
@@ -3,7 +3,7 @@
     internal class Apikeys
     {
         public readonly string ApiKeyE621 = "Api-key-here";
-        public readonly string username = "Username-here";
+        public readonly string username = "Username-E621-here";
         public readonly string useragent = "UserE621-agent-here";
         public readonly string ApiKeytele = "Api-key-here";
     }
